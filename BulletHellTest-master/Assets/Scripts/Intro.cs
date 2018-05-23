590e46c1bfbd9d230fdf3c3f355a02bf55522cb3
jorge701 <jorgemarcosilvestre@gmail.com>
2018-05-23 16:29:48 +0200

    cambios
    integrar interfaz

---
+++ BulletHellTest-master/Assets/Scripts/Intro.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Intro : MonoBehaviour
+{
+
+    public CanvasGroup uiElement;
+
+    public void FadeIn()
+    {
+        StartCoroutine(FadeCanvaGroup(uiElement, uiElement.alpha, 1));
+    }
+
+    public IEnumerator FadeCanvaGroup(CanvasGroup cg, float start, float end, float lerpTime = 0.5f)
+    {
+        while (true)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        print("done");
+
+    }
+
+} 
